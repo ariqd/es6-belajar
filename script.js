2f31343828e2015500b7e673fb89b5e5a98674be
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-07 11:48:05 +0700

    belajar foreach

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("hello");
+// Ambil Element
+const skills = document.getElementById("skills");
+
+// Buat variabel
+const mySkills = ["ES6", "UX Design", "Git"];
+
+// Normal function
+function showSkill(mySkill) {
+  console.log(mySkill);
+}
+
+// Arrow Function
+const showSkill2 = (mySkill) => {
+  console.log(mySkill);
+};
+
+// forEach
+mySkills.forEach(function (mySkill) {
+  console.log(mySkill);
+});
+
+let parent = "<ul>";
+
+mySkills.forEach((mySkill) => {
+  //   console.log(mySkill);
+  parent += `<li>${mySkill}</li>`;
+});
+
+parent += "</ul>";
+
+mySkills.forEach(showSkill2);
+skills.innerHTML = parent;
