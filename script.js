d77abce53ec01d6ad26f10ea14d3a7db6870764f
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-07 13:20:48 +0700

    Learning map and filter

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("hello");
+// Ambil element
+const skills = document.getElementById("skills");
+const primarySkill = document.getElementById("primarySkill");
+
+const mySkills = ["ES6", "Git", "Web Development", "Web Design"];
+
+// Map
+// Undefined
+const printSkillsForEach = mySkills.forEach((skill) => {
+  return skill;
+});
+
+// Benar
+const printSkillsMap = mySkills.map((skill) => {
+  return skill;
+});
+
+skills.innerHTML = printSkillsMap;
+
+// Filter
+const printPrimarySkill = mySkills.filter((skill) => {
+  return skill !== "ES6";
+});
+
+primarySkill.innerHTML = printPrimarySkill;
