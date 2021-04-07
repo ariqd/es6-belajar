ccdeddeba804de45a992ad5b4f896ae91be448e7
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-07 10:57:41 +0700

    Belajar Let

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("hello");
+// ES5
+// var myName = "Ariq";
+// myName = "Richard";
+// console.log(myName);
+
+var myCarsVar = ["BMW", "Tesla", "Toyota"];
+
+for (var i = 0; i < myCarsVar.length; i++) {
+  console.log("My Cars var = " + myCarsVar[i]);
+}
+
+console.log(i);
+
+// ES6
+// let myNameLet = "Ariq"
+// myNameLet = "Calvin"
+// console.log(myNameLet)
+
+const myCarsLet = ["BMW", "Tesla", "Toyota"];
+
+for (let x = 0; x < myCarsLet.length; x++) {
+  console.log("My Cars let = " + myCarsLet[x]);
+}
+
+console.log(x);
+
+// function abc() {
+//   var myNameFunc = "hello";
+//   console.log(myNameFunc);
+// }
+
+// abc();
+// console.log(myNameFunc);
