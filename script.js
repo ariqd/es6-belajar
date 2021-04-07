43ba7ab70644c6aa2bc0cf4dc3614a865efdd168
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-07 11:20:26 +0700

    belajar object literal

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello");
+// OBJECT LITERALS
+const cart = document.getElementById("cart");
+
+// ES5
+function addProduct(name, category) {
+  return {
+    name: name,
+    category: category,
+  };
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone");
+console.log(getProduct);
+cart.innerHTML = `Product 1: ${getProduct.name}, category ${getProduct.category}`;
+
+// ES6
+function addProduct(name, category) {
+  return { name, category };
+}
+
+var getProduct = addProduct("iPhone 12", "Smartphone");
+console.log(getProduct);
+cart.innerHTML = `Product 1: ${getProduct.productName}, category ${getProduct.category}`;
