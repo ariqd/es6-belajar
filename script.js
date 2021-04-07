06348305e076dfecf820bab6ae305678a1db7ed0
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-07 11:06:47 +0700

    belajar string concatenation

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("hello");
+// Ambil element User
+const user = document.getElementById("user");
+
+// Init variable
+const user_logged_in = "Marcell";
+
+// ES5
+// user.innerHTML = "Selamat datang, " + user_logged_in;
+
+// ES6
+user.innerHTML = `Anda berhasil login, ${user_logged_in}`;
