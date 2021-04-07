e92d0b7c19cf56bc48b930aaaf8b2ec04c8d3993
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-04-07 10:31:57 +0700

    Var VS Const

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log(1)
+// ES5
+var myName = "Ariq"
+myName = "Rifki"
+console.log(myName)
+
+// ES6
+// const / constant
+const myCar = "Mazda EX-7"
+// const myCar = "Honda Freed"
+// myCar = "Honda ABC" 
+console.log(myCar)
